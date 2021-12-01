dbc23bab3aeec908e552bc6fb59597685c030d4a
Gabriel Fernandes Rosa Bojikian <gabriel.bojikian.dev@gmail.com>
2021-11-30 21:38:22 -0300

    Refactor: Reorganização e ajuste
    A proteção foi ajustada para detectar corretamente os defeitos elétricos
    dos sinais aquisitados, bem como o momento de atuação da proteção.
    
    Algumas funções foram refatoradas para melhorar a sua reutilizabilidade.

---
+++ protecao_diferencial.m
@@ -0,0 +1,24 @@
+function [sinal_de_atuacao] = protecao_diferencial(Ilocal, Iremoto, IopMin, k, animacao = false)
+    correnteDeOperacao = [];
+    correnteDeResistencia = [];
+    atuacao = [];
+    for i=1:length(Ilocal)
+        correnteDeOperacao(i) = abs(Ilocal(i).complex - Iremoto(i).complex);
+        correnteDeResistencia(i) = (Ilocal(i).magnitude + Iremoto(i).magnitude)/2;
+        if( correnteDeOperacao(i) > IopMin && correnteDeOperacao(i) > k*correnteDeResistencia(i) )
+        sinal_de_atuacao(i) = 1;
+        else
+        sinal_de_atuacao(i) = 0;
+        endif
+    endfor
+
+    plota_grafico_diferencial(k, IopMin)%, max(correnteDeResistencia), max(correnteDeOperacao));
+
+    for i=1:length(correnteDeOperacao)
+        plot(correnteDeResistencia(i), correnteDeOperacao(i), 'ob');
+        
+        if( animacao == true)
+            pause(.1);
+        endif
+    endfor
+endfunction
